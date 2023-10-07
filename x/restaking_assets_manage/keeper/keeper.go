6231fd7e100e4e31223107a306f895d019e49755
TimmyExogenous <chuang@exogenous.io>
2023-10-07 09:46:49 +0800

    add init and export genesis in restaking_assets_manage module

--- x/restaking_assets_manage/keeper/keeper.go
+++ x/restaking_assets_manage/keeper/keeper.go
@@ -3,11 +3,40 @@
 package keeper
 
 import (
+	"cosmossdk.io/math"
 	"github.com/cosmos/cosmos-sdk/codec"
 	storetypes "github.com/cosmos/cosmos-sdk/store/types"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	types2 "github.com/exocore/x/restaking_assets_manage/types"
 )
 
 type Keeper struct {
 	storeKey storetypes.StoreKey
 	cdc      codec.BinaryCodec
+}
+
+// IReStakingAssetsManage interface will be implemented by restaking_assets_manage keeper
+type IReStakingAssetsManage interface {
+	SetClientChainInfo(info *types2.ClientChainInfo) (exoCoreChainIndex uint64, err error)
+	GetClientChainInfoByIndex(exoCoreChainIndex uint64) (info types2.ClientChainInfo, err error)
+	GetAllClientChainInfo() (infos map[uint64]types2.ClientChainInfo, err error)
+
+	SetReStakingAssetInfo(info *types2.ReStakingAssetInfo) (exoCoreAssetIndex uint64, err error)
+	GetReStakingAssetInfo(assetId string) (info types2.ReStakingAssetInfo, err error)
+	GetAllReStakingAssetsInfo() (allAssets map[string]types2.ReStakingAssetInfo, err error)
+
+	GetReStakerAssetInfos(reStakerId string) (assetsInfo map[string]math.Uint, err error)
+	GetReStakerSpecifiedAssetAmount(reStakerId string, assetId string) (amount math.Uint, err error)
+	IncreaseReStakerAssetsAmount(reStakerId string, assetsAddAmount map[string]math.Uint) (err error)
+	DecreaseReStakerAssetsAmount(reStakerId string, assetsSubAmount map[string]math.Uint) (err error)
+
+	GetOperatorAssetInfos(operatorAddr sdk.Address) (assetsInfo map[string]math.Uint, err error)
+	GetOperatorSpecifiedAssetAmount(operatorAddr sdk.Address, assetId string) (amount math.Uint, err error)
+	IncreaseOperatorAssetsAmount(operatorAddr sdk.Address, assetsAddAmount map[string]math.Uint) (err error)
+	DecreaseOperatorAssetsAmount(operatorAddr sdk.Address, assetsSubAmount map[string]math.Uint) (err error)
+	GetOperatorAssetOptedInMiddleWare(operatorAddr sdk.Address, assetId string) (middleWares []sdk.Address, err error)
+
+	// GetAllOperatorAssetOptedInMiddleWare can also be implemented in operator optedIn module
+	GetAllOperatorAssetOptedInMiddleWare(operatorAddr sdk.Address) (optedInInfos map[string][]sdk.Address, err error)
+	SetOperatorAssetOptedInMiddleWare(operatorAddr sdk.Address, setInfo map[string]sdk.Address) (middleWares []sdk.Address, err error)
 }
--- x/restaking_assets_manage/module.go
+++ x/restaking_assets_manage/module.go
@@ -3,16 +3,16 @@
 package restaking_assets_manage
 
 import (
-	"cosmossdk.io/math"
 	"encoding/json"
 	"fmt"
+	abci "github.com/cometbft/cometbft/abci/types"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/codec"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/cosmos-sdk/types/module"
 	simtypes "github.com/cosmos/cosmos-sdk/types/simulation"
-	"github.com/exocore/x/deposit/keeper"
+	"github.com/exocore/x/restaking_assets_manage/keeper"
 	types2 "github.com/exocore/x/restaking_assets_manage/types"
 	"github.com/grpc-ecosystem/grpc-gateway/runtime"
 	"github.com/spf13/cobra"
@@ -42,17 +42,17 @@ func (b AppModuleBasic) RegisterLegacyAminoCodec(amino *codec.LegacyAmino) {
 // DefaultGenesis returns default genesis state as raw bytes for the auth
 // module.
 func (AppModuleBasic) DefaultGenesis(cdc codec.JSONCodec) json.RawMessage {
-	return cdc.MustMarshalJSON(types.DefaultGenesisState())
+	return cdc.MustMarshalJSON(DefaultGenesisState())
 }
 
 // ValidateGenesis performs genesis state validation for the auth module.
 func (AppModuleBasic) ValidateGenesis(cdc codec.JSONCodec, config client.TxEncodingConfig, bz json.RawMessage) error {
-	var data types.GenesisState
+	var data types2.GenesisState
 	if err := cdc.UnmarshalJSON(bz, &data); err != nil {
-		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types.ModuleName, err)
+		return fmt.Errorf("failed to unmarshal %s genesis state: %w", types2.ModuleName, err)
 	}
 
-	return types.ValidateGenesis(data)
+	return ValidateGenesis(data)
 }
 
 func (b AppModuleBasic) RegisterInterfaces(registry codectypes.InterfaceRegistry) {
@@ -77,7 +77,19 @@ func (b AppModuleBasic) GetQueryCmd() *cobra.Command {
 
 type AppModule struct {
 	AppModuleBasic
-	keeper *keeper.Keeper
+	keeper keeper.Keeper
+}
+
+func (am AppModule) InitGenesis(ctx sdk.Context, cdc codec.JSONCodec, data json.RawMessage) []abci.ValidatorUpdate {
+	var genesisState types2.GenesisState
+	cdc.MustUnmarshalJSON(data, &genesisState)
+	InitGenesis(ctx, am.keeper, genesisState)
+	return []abci.ValidatorUpdate{}
+}
+
+func (am AppModule) ExportGenesis(ctx sdk.Context, cdc codec.JSONCodec) json.RawMessage {
+	gs := ExportGenesis(ctx, am.keeper)
+	return cdc.MustMarshalJSON(gs)
 }
 
 func (am AppModule) GenerateGenesisState(input *module.SimulationState) {
@@ -93,40 +105,4 @@ func (am AppModule) RegisterStoreDecoder(registry sdk.StoreDecoderRegistry) {
 func (am AppModule) WeightedOperations(simState module.SimulationState) []simtypes.WeightedOperation {
 	//TODO implement me
 	panic("implement me")
-}
-
-type ReStakingChainInfo struct {
-	ChainName string
-	ChainId   uint64
-}
-
-type ReStakingTokenInfo struct {
-	TokenAddress string
-	TokenName    string
-}
-
-// IReStakingAssetsManage interface will be implemented by restaking_assets_manage keeper
-type IReStakingAssetsManage interface {
-	SetClientChainInfo(info *types2.ClientChainInfo) (exoCoreChainIndex uint64, err error)
-	GetClientChainInfoByIndex(exoCoreChainIndex uint64) (info types2.ClientChainInfo, err error)
-	GetAllClientChainInfo() (infos map[uint64]types2.ClientChainInfo, err error)
-
-	SetReStakingAssetInfo(info *types2.ReStakingAssetInfo) (exoCoreAssetIndex uint64, err error)
-	GetReStakingAssetInfo(assetId string) (info types2.ReStakingAssetInfo, err error)
-	GetAllReStakingAssetsInfo() (allAssets map[string]types2.ReStakingAssetInfo, err error)
-
-	GetReStakerAssetInfos(reStakerId string) (assetsInfo map[string]math.Uint, err error)
-	GetReStakerSpecifiedAssetAmount(reStakerId string, assetId string) (amount math.Uint, err error)
-	IncreaseReStakerAssetsAmount(reStakerId string, assetsAddAmount map[string]math.Uint) (err error)
-	DecreaseReStakerAssetsAmount(reStakerId string, assetsSubAmount map[string]math.Uint) (err error)
-
-	GetOperatorAssetInfos(operatorAddr sdk.Address) (assetsInfo map[string]math.Uint, err error)
-	GetOperatorSpecifiedAssetAmount(operatorAddr sdk.Address, assetId string) (amount math.Uint, err error)
-	IncreaseOperatorAssetsAmount(operatorAddr sdk.Address, assetsAddAmount map[string]math.Uint) (err error)
-	DecreaseOperatorAssetsAmount(operatorAddr sdk.Address, assetsSubAmount map[string]math.Uint) (err error)
-	GetOperatorAssetOptedInMiddleWare(operatorAddr sdk.Address, assetId string) (middleWares []sdk.Address, err error)
-
-	// GetAllOperatorAssetOptedInMiddleWare can also be implemented in operator optedIn module
-	GetAllOperatorAssetOptedInMiddleWare(operatorAddr sdk.Address) (optedInInfos map[string][]sdk.Address, err error)
-	SetOperatorAssetOptedInMiddleWare(operatorAddr sdk.Address, setInfo map[string]sdk.Address) (middleWares []sdk.Address, err error)
 }
