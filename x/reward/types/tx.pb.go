9848be2b12269d6dc5cf0efcd5b965df4505206c
mikebraver <kimmyhu566@gmail.com>
2023-10-26 23:17:12 +0800

    update

--- x/reward/types/tx.pb.go
+++ x/reward/types/tx.pb.go
@@ -123,15 +123,105 @@ func (m *MsgClaimRewardRequestResponse) XXX_DiscardUnknown() {
 
 var xxx_messageInfo_MsgClaimRewardRequestResponse proto.InternalMessageInfo
 
+type MsgRewardDetail struct {
+	Creator string `protobuf:"bytes,1,opt,name=creator,proto3" json:"creator,omitempty"`
+	Id      uint64 `protobuf:"varint,2,opt,name=id,proto3" json:"id,omitempty"`
+}
+
+func (m *MsgRewardDetail) Reset()         { *m = MsgRewardDetail{} }
+func (m *MsgRewardDetail) String() string { return proto.CompactTextString(m) }
+func (*MsgRewardDetail) ProtoMessage()    {}
+func (*MsgRewardDetail) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9cd4863caedb1c8f, []int{2}
+}
+func (m *MsgRewardDetail) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgRewardDetail) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgRewardDetail.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgRewardDetail) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgRewardDetail.Merge(m, src)
+}
+func (m *MsgRewardDetail) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgRewardDetail) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgRewardDetail.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgRewardDetail proto.InternalMessageInfo
+
+func (m *MsgRewardDetail) GetCreator() string {
+	if m != nil {
+		return m.Creator
+	}
+	return ""
+}
+
+func (m *MsgRewardDetail) GetId() uint64 {
+	if m != nil {
+		return m.Id
+	}
+	return 0
+}
+
+type MsgRewardDetailResponse struct {
+}
+
+func (m *MsgRewardDetailResponse) Reset()         { *m = MsgRewardDetailResponse{} }
+func (m *MsgRewardDetailResponse) String() string { return proto.CompactTextString(m) }
+func (*MsgRewardDetailResponse) ProtoMessage()    {}
+func (*MsgRewardDetailResponse) Descriptor() ([]byte, []int) {
+	return fileDescriptor_9cd4863caedb1c8f, []int{3}
+}
+func (m *MsgRewardDetailResponse) XXX_Unmarshal(b []byte) error {
+	return m.Unmarshal(b)
+}
+func (m *MsgRewardDetailResponse) XXX_Marshal(b []byte, deterministic bool) ([]byte, error) {
+	if deterministic {
+		return xxx_messageInfo_MsgRewardDetailResponse.Marshal(b, m, deterministic)
+	} else {
+		b = b[:cap(b)]
+		n, err := m.MarshalToSizedBuffer(b)
+		if err != nil {
+			return nil, err
+		}
+		return b[:n], nil
+	}
+}
+func (m *MsgRewardDetailResponse) XXX_Merge(src proto.Message) {
+	xxx_messageInfo_MsgRewardDetailResponse.Merge(m, src)
+}
+func (m *MsgRewardDetailResponse) XXX_Size() int {
+	return m.Size()
+}
+func (m *MsgRewardDetailResponse) XXX_DiscardUnknown() {
+	xxx_messageInfo_MsgRewardDetailResponse.DiscardUnknown(m)
+}
+
+var xxx_messageInfo_MsgRewardDetailResponse proto.InternalMessageInfo
+
 func init() {
 	proto.RegisterType((*MsgClaimRewardRequest)(nil), "exocore.reward.MsgClaimRewardRequest")
 	proto.RegisterType((*MsgClaimRewardRequestResponse)(nil), "exocore.reward.MsgClaimRewardRequestResponse")
+	proto.RegisterType((*MsgRewardDetail)(nil), "exocore.reward.MsgRewardDetail")
+	proto.RegisterType((*MsgRewardDetailResponse)(nil), "exocore.reward.MsgRewardDetailResponse")
 }
 
 func init() { proto.RegisterFile("exocore/reward/tx.proto", fileDescriptor_9cd4863caedb1c8f) }
 
 var fileDescriptor_9cd4863caedb1c8f = []byte{
-	// 230 bytes of a gzipped FileDescriptorProto
+	// 276 bytes of a gzipped FileDescriptorProto
 	0x1f, 0x8b, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0xff, 0xe2, 0x12, 0x4f, 0xad, 0xc8, 0x4f,
 	0xce, 0x2f, 0x4a, 0xd5, 0x2f, 0x4a, 0x2d, 0x4f, 0x2c, 0x4a, 0xd1, 0x2f, 0xa9, 0xd0, 0x2b, 0x28,
 	0xca, 0x2f, 0xc9, 0x17, 0xe2, 0x83, 0x4a, 0xe8, 0x41, 0x24, 0x94, 0xd2, 0xb9, 0x44, 0x7d, 0x8b,
@@ -140,13 +230,16 @@ var fileDescriptor_9cd4863caedb1c8f = []byte{
 	0x9c, 0x41, 0x30, 0xae, 0x10, 0x1f, 0x17, 0x53, 0x66, 0x8a, 0x04, 0x93, 0x02, 0xa3, 0x06, 0x4b,
 	0x10, 0x53, 0x66, 0x8a, 0x90, 0x0a, 0x17, 0x2f, 0xc4, 0x30, 0xc7, 0x94, 0x94, 0xa2, 0xd4, 0xe2,
 	0x62, 0x09, 0x66, 0xb0, 0x7a, 0x54, 0x41, 0x25, 0x79, 0x2e, 0x59, 0xac, 0x16, 0x05, 0xa5, 0x16,
-	0x17, 0xe4, 0xe7, 0x15, 0xa7, 0x1a, 0x15, 0x72, 0x31, 0xfb, 0x16, 0xa7, 0x0b, 0x65, 0x71, 0x09,
-	0x61, 0x71, 0x8d, 0xaa, 0x1e, 0xaa, 0xbb, 0xf5, 0xb0, 0x9a, 0x25, 0xa5, 0x4b, 0x94, 0x32, 0x98,
-	0x95, 0x4e, 0xd6, 0x27, 0x1e, 0xc9, 0x31, 0x5e, 0x78, 0x24, 0xc7, 0xf8, 0xe0, 0x91, 0x1c, 0xe3,
-	0x84, 0xc7, 0x72, 0x0c, 0x17, 0x1e, 0xcb, 0x31, 0xdc, 0x78, 0x2c, 0xc7, 0x10, 0xa5, 0x98, 0x9e,
-	0x59, 0x92, 0x51, 0x9a, 0xa4, 0x97, 0x9c, 0x9f, 0xab, 0x0f, 0x0b, 0xca, 0x0a, 0x78, 0x60, 0x56,
-	0x16, 0xa4, 0x16, 0x27, 0xb1, 0x81, 0x03, 0xd4, 0x18, 0x10, 0x00, 0x00, 0xff, 0xff, 0x3b, 0x7a,
-	0x24, 0x79, 0x6b, 0x01, 0x00, 0x00,
+	0x17, 0xe4, 0xe7, 0x15, 0xa7, 0x2a, 0x59, 0x73, 0xf1, 0xfb, 0x16, 0xa7, 0x43, 0xe4, 0x5c, 0x52,
+	0x4b, 0x12, 0x33, 0x73, 0x88, 0x77, 0x83, 0x92, 0x24, 0x97, 0x38, 0x9a, 0x66, 0x98, 0xb9, 0x46,
+	0xa7, 0x19, 0xb9, 0x98, 0x7d, 0x8b, 0xd3, 0x85, 0xb2, 0xb8, 0x84, 0xb0, 0x78, 0x53, 0x55, 0x0f,
+	0x35, 0x40, 0xf4, 0xb0, 0x3a, 0x52, 0x4a, 0x97, 0x28, 0x65, 0x30, 0x3b, 0x85, 0x22, 0xb8, 0x78,
+	0x50, 0x3c, 0x22, 0x8f, 0x45, 0x3b, 0xb2, 0x02, 0x29, 0x75, 0x02, 0x0a, 0x60, 0x26, 0x3b, 0x59,
+	0x9f, 0x78, 0x24, 0xc7, 0x78, 0xe1, 0x91, 0x1c, 0xe3, 0x83, 0x47, 0x72, 0x8c, 0x13, 0x1e, 0xcb,
+	0x31, 0x5c, 0x78, 0x2c, 0xc7, 0x70, 0xe3, 0xb1, 0x1c, 0x43, 0x94, 0x62, 0x7a, 0x66, 0x49, 0x46,
+	0x69, 0x92, 0x5e, 0x72, 0x7e, 0xae, 0x3e, 0x2c, 0xf6, 0x2b, 0xe0, 0xf1, 0x5f, 0x59, 0x90, 0x5a,
+	0x9c, 0xc4, 0x06, 0x4e, 0x03, 0xc6, 0x80, 0x00, 0x00, 0x00, 0xff, 0xff, 0xd4, 0x9c, 0xa3, 0x42,
+	0x1e, 0x02, 0x00, 0x00,
 }
 
 // Reference imports to suppress errors if they are not otherwise used.
@@ -162,6 +255,7 @@ const _ = grpc.SupportPackageIsVersion4
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://godoc.org/google.golang.org/grpc#ClientConn.NewStream.
 type MsgClient interface {
 	ClaimRewardRequest(ctx context.Context, in *MsgClaimRewardRequest, opts ...grpc.CallOption) (*MsgClaimRewardRequestResponse, error)
+	RewardDetail(ctx context.Context, in *MsgRewardDetail, opts ...grpc.CallOption) (*MsgRewardDetailResponse, error)
 }
 
 type msgClient struct {
@@ -181,9 +275,19 @@ func (c *msgClient) ClaimRewardRequest(ctx context.Context, in *MsgClaimRewardRe
 	return out, nil
 }
 
+func (c *msgClient) RewardDetail(ctx context.Context, in *MsgRewardDetail, opts ...grpc.CallOption) (*MsgRewardDetailResponse, error) {
+	out := new(MsgRewardDetailResponse)
+	err := c.cc.Invoke(ctx, "/exocore.reward.Msg/RewardDetail", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // MsgServer is the server API for Msg service.
 type MsgServer interface {
 	ClaimRewardRequest(context.Context, *MsgClaimRewardRequest) (*MsgClaimRewardRequestResponse, error)
+	RewardDetail(context.Context, *MsgRewardDetail) (*MsgRewardDetailResponse, error)
 }
 
 // UnimplementedMsgServer can be embedded to have forward compatible implementations.
@@ -192,6 +296,9 @@ type UnimplementedMsgServer struct {
 
 func (*UnimplementedMsgServer) ClaimRewardRequest(ctx context.Context, req *MsgClaimRewardRequest) (*MsgClaimRewardRequestResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method ClaimRewardRequest not implemented")
+}
+func (*UnimplementedMsgServer) RewardDetail(ctx context.Context, req *MsgRewardDetail) (*MsgRewardDetailResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method RewardDetail not implemented")
 }
 
 func RegisterMsgServer(s grpc1.Server, srv MsgServer) {
@@ -216,6 +323,24 @@ func _Msg_ClaimRewardRequest_Handler(srv interface{}, ctx context.Context, dec f
 	return interceptor(ctx, in, info, handler)
 }
 
+func _Msg_RewardDetail_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(MsgRewardDetail)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(MsgServer).RewardDetail(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/exocore.reward.Msg/RewardDetail",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(MsgServer).RewardDetail(ctx, req.(*MsgRewardDetail))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 var _Msg_serviceDesc = grpc.ServiceDesc{
 	ServiceName: "exocore.reward.Msg",
 	HandlerType: (*MsgServer)(nil),
@@ -223,6 +348,10 @@ var _Msg_serviceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "ClaimRewardRequest",
 			Handler:    _Msg_ClaimRewardRequest_Handler,
+		},
+		{
+			MethodName: "RewardDetail",
+			Handler:    _Msg_RewardDetail_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
@@ -294,6 +423,64 @@ func (m *MsgClaimRewardRequestResponse) MarshalToSizedBuffer(dAtA []byte) (int, 
 	return len(dAtA) - i, nil
 }
 
+func (m *MsgRewardDetail) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgRewardDetail) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgRewardDetail) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	if m.Id != 0 {
+		i = encodeVarintTx(dAtA, i, uint64(m.Id))
+		i--
+		dAtA[i] = 0x10
+	}
+	if len(m.Creator) > 0 {
+		i -= len(m.Creator)
+		copy(dAtA[i:], m.Creator)
+		i = encodeVarintTx(dAtA, i, uint64(len(m.Creator)))
+		i--
+		dAtA[i] = 0xa
+	}
+	return len(dAtA) - i, nil
+}
+
+func (m *MsgRewardDetailResponse) Marshal() (dAtA []byte, err error) {
+	size := m.Size()
+	dAtA = make([]byte, size)
+	n, err := m.MarshalToSizedBuffer(dAtA[:size])
+	if err != nil {
+		return nil, err
+	}
+	return dAtA[:n], nil
+}
+
+func (m *MsgRewardDetailResponse) MarshalTo(dAtA []byte) (int, error) {
+	size := m.Size()
+	return m.MarshalToSizedBuffer(dAtA[:size])
+}
+
+func (m *MsgRewardDetailResponse) MarshalToSizedBuffer(dAtA []byte) (int, error) {
+	i := len(dAtA)
+	_ = i
+	var l int
+	_ = l
+	return len(dAtA) - i, nil
+}
+
 func encodeVarintTx(dAtA []byte, offset int, v uint64) int {
 	offset -= sovTx(v)
 	base := offset
@@ -326,6 +513,31 @@ func (m *MsgClaimRewardRequest) Size() (n int) {
 }
 
 func (m *MsgClaimRewardRequestResponse) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	return n
+}
+
+func (m *MsgRewardDetail) Size() (n int) {
+	if m == nil {
+		return 0
+	}
+	var l int
+	_ = l
+	l = len(m.Creator)
+	if l > 0 {
+		n += 1 + l + sovTx(uint64(l))
+	}
+	if m.Id != 0 {
+		n += 1 + sovTx(uint64(m.Id))
+	}
+	return n
+}
+
+func (m *MsgRewardDetailResponse) Size() (n int) {
 	if m == nil {
 		return 0
 	}
@@ -500,6 +712,157 @@ func (m *MsgClaimRewardRequestResponse) Unmarshal(dAtA []byte) error {
 		}
 		if fieldNum <= 0 {
 			return fmt.Errorf("proto: MsgClaimRewardRequestResponse: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgRewardDetail) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgRewardDetail: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgRewardDetail: illegal tag %d (wire type %d)", fieldNum, wire)
+		}
+		switch fieldNum {
+		case 1:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Creator", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return ErrInvalidLengthTx
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return ErrInvalidLengthTx
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.Creator = string(dAtA[iNdEx:postIndex])
+			iNdEx = postIndex
+		case 2:
+			if wireType != 0 {
+				return fmt.Errorf("proto: wrong wireType = %d for field Id", wireType)
+			}
+			m.Id = 0
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return ErrIntOverflowTx
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				m.Id |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+		default:
+			iNdEx = preIndex
+			skippy, err := skipTx(dAtA[iNdEx:])
+			if err != nil {
+				return err
+			}
+			if (skippy < 0) || (iNdEx+skippy) < 0 {
+				return ErrInvalidLengthTx
+			}
+			if (iNdEx + skippy) > l {
+				return io.ErrUnexpectedEOF
+			}
+			iNdEx += skippy
+		}
+	}
+
+	if iNdEx > l {
+		return io.ErrUnexpectedEOF
+	}
+	return nil
+}
+func (m *MsgRewardDetailResponse) Unmarshal(dAtA []byte) error {
+	l := len(dAtA)
+	iNdEx := 0
+	for iNdEx < l {
+		preIndex := iNdEx
+		var wire uint64
+		for shift := uint(0); ; shift += 7 {
+			if shift >= 64 {
+				return ErrIntOverflowTx
+			}
+			if iNdEx >= l {
+				return io.ErrUnexpectedEOF
+			}
+			b := dAtA[iNdEx]
+			iNdEx++
+			wire |= uint64(b&0x7F) << shift
+			if b < 0x80 {
+				break
+			}
+		}
+		fieldNum := int32(wire >> 3)
+		wireType := int(wire & 0x7)
+		if wireType == 4 {
+			return fmt.Errorf("proto: MsgRewardDetailResponse: wiretype end group for non-group")
+		}
+		if fieldNum <= 0 {
+			return fmt.Errorf("proto: MsgRewardDetailResponse: illegal tag %d (wire type %d)", fieldNum, wire)
 		}
 		switch fieldNum {
 		default:
