0d51762130531a7bcbfba3355cdc69acaa786315
mikebraver <142298428+mikebraver@users.noreply.github.com>
2023-11-22 14:26:01 +0800

    update cli command

--- x/exoslash/client/cli/tx.go
+++ x/exoslash/client/cli/tx.go
@@ -7,6 +7,8 @@ import (
 	"github.com/spf13/cobra"
 
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
+	"github.com/cosmos/cosmos-sdk/client/tx"
 	"github.com/exocore/x/exoslash/types"
 )
 
@@ -29,7 +31,41 @@ func GetTxCmd() *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	// this line is used by starport scaffolding # 1
+	cmd.AddCommand(
+		UpdateParams(),
+	)
 
+	return cmd
+}
+
+// UpdateParams todo: it should be a gov proposal command in future.
+func UpdateParams() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "UpdateParams ExoCoreLZAppAddr ExoCoreLzAppEventTopic",
+		Short: "set ExoCoreLZAppAddr and ExoCoreLzAppEventTopic params to exoSlash module",
+		Args:  cobra.ExactArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			cliCtx, err := client.GetClientTxContext(cmd)
+			if err != nil {
+				return err
+			}
+
+			sender := cliCtx.GetFromAddress()
+			msg := &types.MsgUpdateParams{
+				Authority: sender.String(),
+				Params: types.Params{
+					ExoCoreLzAppAddress:    args[0],
+					ExoCoreLzAppEventTopic: args[1],
+				},
+			}
+
+			if err := msg.ValidateBasic(); err != nil {
+				return err
+			}
+			return tx.GenerateOrBroadcastTxCLI(cliCtx, cmd.Flags(), msg)
+		},
+	}
+
+	flags.AddTxFlagsToCmd(cmd)
 	return cmd
 }
