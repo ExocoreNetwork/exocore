15e8e8af96496b6621703bd49a2cdadd7c000dc8
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-02-06 16:10:45 +0000

    refactor(dogfood): remove errors

--- x/dogfood/types/errors.go
+++ x/dogfood/types/errors.go
@@ -1,12 +1,3 @@
 package types
 
 // DONTCOVER
-
-import (
-	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
-)
-
-// x/dogfood module sentinel errors
-var (
-	ErrSample = sdkerrors.Register(ModuleName, 1100, "sample error")
-)
