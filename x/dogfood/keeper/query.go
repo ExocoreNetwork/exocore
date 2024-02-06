92be5654213c26e3530d84733127052ad8883816
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-02-06 16:14:59 +0000

    refactor(dogfood): merge `query_params.go`

--- x/dogfood/keeper/query.go
+++ x/dogfood/keeper/query.go
@@ -1,7 +1,25 @@
 package keeper
 
 import (
+	"context"
+
 	"github.com/ExocoreNetwork/exocore/x/dogfood/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var _ types.QueryServer = Keeper{}
+
+func (k Keeper) Params(
+	goCtx context.Context,
+	req *types.QueryParamsRequest,
+) (*types.QueryParamsResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	return &types.QueryParamsResponse{Params: k.GetParams(ctx)}, nil
+}
