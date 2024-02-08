a2aa2e106c43e60d908526a928b8fa035ba7391f
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-02-08 16:54:31 +0000

    fix(dogfood): call `EndBlock` from module

--- x/dogfood/module.go
+++ x/dogfood/module.go
@@ -159,6 +159,6 @@ func (AppModule) ConsensusVersion() uint64 { return 1 }
 func (am AppModule) BeginBlock(_ sdk.Context, _ abci.RequestBeginBlock) {}
 
 // EndBlock contains the logic that is automatically triggered at the end of each block
-func (am AppModule) EndBlock(_ sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	return []abci.ValidatorUpdate{}
+func (am AppModule) EndBlock(ctx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
+	return am.keeper.EndBlock(ctx)
 }
