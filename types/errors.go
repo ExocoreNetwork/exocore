2e0f655a9498586c0605a18cae6d5314001c89ca
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-02-20 08:42:15 +0000

    chore(lint): remove superfluous file
    The file exists in ExocoreNetwork/evmos so it is not needed here.

--- types/errors.go
+++
@@ -1,7 +0,0 @@
-package types
-
-// RootCodespace is the codespace for all errors defined in this package
-// const RootCodespace = "evmos"
-
-// ErrInvalidChainID returns an error resulting from an invalid chain ID.
-// var ErrInvalidChainID = errorsmod.Register(RootCodespace, 3, "invalid chain ID")
