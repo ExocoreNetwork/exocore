72e1a6ffa34c0e4bae37e0faeb9b4f40a5f04d89
TimmyExogenous <chuang@exogenous.io>
2024-02-28 15:19:34 +0800

    delete the code related to setting ExoCoreLzAppAddress to fix the bug when set parameter

--- x/withdraw/client/cli/query.go
+++ x/withdraw/client/cli/query.go
@@ -24,7 +24,7 @@ func GetQueryCmd(string) *cobra.Command {
 		RunE:                       client.ValidateCmd,
 	}
 
-	cmd.AddCommand(CmdQueryParams())
+	cmd.AddCommand()
 	// this line is used by starport scaffolding # 1
 
 	return cmd
