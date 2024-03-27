faae2960229d6afda5a22fb2f0b423e01c92fa51
mikebraver <142298428+mikebraver@users.noreply.github.com>
2024-03-28 03:19:50 +0800

    add avs manager precompiles

--- x/avs/keeper/msg_server.go
+++ x/avs/keeper/msg_server.go
@@ -2,34 +2,34 @@ package keeper
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/ExocoreNetwork/exocore/x/avs/types"
-	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
 var _ types.MsgServer = &Keeper{}
 
 func (k Keeper) RegisterAVS(ctx context.Context, req *types.RegisterAVSReq) (*types.RegisterAVSResponse, error) {
-	c := sdk.UnwrapSDKContext(ctx)
-	fromAddress := req.FromAddress
-	operatorAddress := req.Info.OperatorAddress
-	for _, opAddr := range operatorAddress {
-		if fromAddress == opAddr {
-			// Set purely for AVS itself information.
-			if err := k.SetAVSInfo(c, req.Info); err != nil {
-				return nil, err
-			}
-		}
-	}
-	return nil, fmt.Errorf("The fromAddress %s is different from operatorAddress %s ", fromAddress, operatorAddress)
+	// Disable cosmos transaction temporarily
+	// c := sdk.UnwrapSDKContext(ctx)
+	// fromAddress := req.FromAddress
+	// operatorAddress := req.Info.OperatorAddress
+	// for _, opAddr := range operatorAddress {
+	// 	if fromAddress == opAddr {
+	// 		// Set purely for AVS itself information.
+	// 		if err := k.SetAVSInfo(c, req.Info); err != nil {
+	// 			return nil, err
+	// 		}
+	// 	}
+	// }
+	return nil, nil
 }
 
 func (k Keeper) DeRegisterAVS(ctx context.Context, req *types.DeRegisterAVSReq) (*types.DeRegisterAVSResponse, error) {
-	c := sdk.UnwrapSDKContext(ctx)
-	if err := k.DeleteAVSInfo(c, req.Info); err != nil {
-		return nil, err
-	}
+	// Disable cosmos transaction temporarily
+	// c := sdk.UnwrapSDKContext(ctx)
+	// if err := k.DeleteAVSInfo(c, req.Info); err != nil {
+	// 	return nil, err
+	// }
 
 	return nil, nil
 }
--- x/avs/keeper/params.go
+++ x/avs/keeper/params.go
@@ -7,8 +7,7 @@ import (
 
 // GetParams get all parameters as types.Params
 func (k Keeper) GetParams(ctx sdk.Context) types.Params {
-	return types.NewParams(
-	)
+	return types.NewParams()
 }
 
 // SetParams set the params
@@ -16,3 +15,14 @@ func (k Keeper) SetParams(ctx sdk.Context, params types.Params) {
 	k.paramstore.SetParamSet(ctx, &params)
 }
 
+type AVSParams struct {
+	AVSName         string
+	AVSAddress      []byte
+	OperatorAddress []byte
+	Action          uint64
+}
+
+const (
+	RegisterAction = iota + 1
+	DeRegisterAction
+)
