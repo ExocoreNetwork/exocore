1a1d0635731572d66323081144f015513bef4ea0
leonz789 <zleon789@gmail.com>
2024-04-03 03:05:24 +0800

    feat(oracle-keeper):implement keeper for state access, msgserver for price update

---
+++ x/oracle/keeper/common/common_test.go
@@ -0,0 +1,29 @@
+package common
+
+import (
+	"math/big"
+	"testing"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	. "github.com/smartystreets/goconvey/convey"
+	"go.uber.org/mock/gomock"
+)
+
+//go:generate mockgen -destination mock_keeper_test.go -package common github.com/ExocoreNetwork/exocore/x/oracle/keeper/common KeeperOracle
+
+//go:generate mockgen -destination mock_validator_test.go -package common github.com/cosmos/cosmos-sdk/x/staking/types ValidatorI
+
+func TestMock(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+	ko := NewMockKeeperOracle(ctrl)
+
+	ko.EXPECT().GetLastTotalPower(gomock.Any()).Return(big.NewInt(99))
+
+	x := ko.GetLastTotalPower(sdk.Context{})
+	_ = x
+
+	Convey("mock oracle keeper", t, func() {
+		Convey("GetLastTotalPower", func() { So(x, ShouldResemble, big.NewInt(99)) })
+	})
+}
