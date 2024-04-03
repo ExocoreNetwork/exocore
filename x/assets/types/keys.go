0fcacf7d5fbad7a04e7c3b45a7f9f60eed02fd2e
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-04-03 12:30:02 +0000

    fix(assets): add ParseID function

--- x/assets/types/keys.go
+++ x/assets/types/keys.go
@@ -7,6 +7,7 @@ import (
 	errorsmod "cosmossdk.io/errors"
 	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 )
 
 // constants
@@ -134,7 +135,31 @@ func ParseJoinedKey(key []byte) (keys []string, err error) {
 func ParseJoinedStoreKey(key []byte, number int) (keys []string, err error) {
 	stringList := strings.Split(string(key), "/")
 	if len(stringList) != number {
-		return nil, errorsmod.Wrap(ErrParseAssetsStateKey, fmt.Sprintf("expected length:%d,actual length:%d,the stringList is:%v", number, len(stringList), stringList))
+		return nil, errorsmod.Wrap(
+			ErrParseAssetsStateKey,
+			fmt.Sprintf(
+				"expected length:%d,actual length:%d,the stringList is:%v",
+				number,
+				len(stringList),
+				stringList,
+			),
+		)
 	}
 	return stringList, nil
+}
+
+func ParseID(key string) (string, uint64, error) {
+	keys := strings.Split(key, "_")
+	if len(keys) != 2 {
+		return "", 0, errorsmod.Wrap(ErrParseAssetsStateKey, fmt.Sprintf("invalid length:%s", key))
+	}
+	if len(keys[0]) == 0 {
+		return "", 0, errorsmod.Wrap(ErrParseAssetsStateKey, fmt.Sprintf("empty key:%s", key))
+	}
+	var id uint64
+	var err error
+	if id, err = hexutil.DecodeUint64(keys[1]); err != nil {
+		return "", 0, errorsmod.Wrap(ErrParseAssetsStateKey, fmt.Sprintf("not a number :%s", key))
+	}
+	return keys[0], id, nil
 }
