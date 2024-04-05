551b95662e5874fa7538394d917a4593f422f2a6
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-04-05 16:03:48 +0000

    fix(dogfood): print the param at panic

--- x/dogfood/keeper/genesis.go
+++ x/dogfood/keeper/genesis.go
@@ -18,11 +18,12 @@ func (k Keeper) InitGenesis(
 	// the `params` validator is not super useful to validate state level information
 	// so, it must be done here. by extension, the `InitGenesis` of the epochs module
 	// should be called before that of this module.
-	_, found := k.epochsKeeper.GetEpochInfo(ctx, genState.Params.EpochIdentifier)
+	epochID := genState.Params.EpochIdentifier
+	_, found := k.epochsKeeper.GetEpochInfo(ctx, epochID)
 	if !found {
 		// the panic is suitable here because it is being done at genesis, when the node
 		// is not running. it means that the genesis file is malformed.
-		panic("epoch info not found")
+		panic(fmt.Sprintf("epoch info not found %s", epochID))
 	}
 	// apply the same logic to the staking assets.
 	for _, assetID := range genState.Params.AssetIDs {
