617ecf1966ac94e106ae6191c0d62c7e5434a556
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-04-28 12:34:37 +0000

    refactor(operator): use flags to RegisterOperator
    Instead of relying on the positioning of arguments, the flags mechanism
    (as seen in the Cosmos SDK's x/staking) offers a cleaner approach.

--- x/operator/client/cli/tx.go
+++ x/operator/client/cli/tx.go
@@ -1,24 +1,31 @@
 package cli
 
 import (
-	"fmt"
 	"strconv"
 	"strings"
 
-	errorsmod "cosmossdk.io/errors"
-	operatortypes "github.com/ExocoreNetwork/exocore/x/operator/types"
-	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
+	flag "github.com/spf13/pflag"
 
+	errorsmod "cosmossdk.io/errors"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/cosmos/cosmos-sdk/client/tx"
+
+	"github.com/ExocoreNetwork/exocore/x/operator/types"
+)
+
+const (
+	FlagApproveAddr     = "approve-addr"
+	FlagMetaInfo        = "meta-info"
+	FlagClientChainData = "client-chain-data"
 )
 
 // NewTxCmd returns a root CLI command handler for deposit commands
 func NewTxCmd() *cobra.Command {
 	txCmd := &cobra.Command{
-		Use:                        operatortypes.ModuleName,
-		Short:                      "operator subcommands",
+		Use:                        types.ModuleName,
+		Short:                      "Operator transaction subcommands",
 		DisableFlagParsing:         true,
 		SuggestionsMinimumDistance: 2,
 		RunE:                       client.ValidateCmd,
@@ -30,52 +37,103 @@ func NewTxCmd() *cobra.Command {
 	return txCmd
 }
 
-// RegisterOperator register to be a operator
+// RegisterOperator returns a CLI command handler for creating a MsgRegisterOperator
+// transaction.
 func RegisterOperator() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "RegisterOperator EarningsAddr ApproveAddr OperatorMetaInfo clientChainLzID:ClientChainEarningsAddr",
-		Short: "register to be a operator",
-		Args:  cobra.MinimumNArgs(4),
+		Use:   "register-operator",
+		Short: "register to become an operator",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			cliCtx, err := client.GetClientTxContext(cmd)
+			clientCtx, err := client.GetClientTxContext(cmd)
 			if err != nil {
 				return err
 			}
 
-			sender := cliCtx.GetFromAddress()
-			msg := &operatortypes.RegisterOperatorReq{
-				FromAddress: sender.String(),
-				Info: &operatortypes.OperatorInfo{
-					EarningsAddr:     args[0],
-					ApproveAddr:      args[1],
-					OperatorMetaInfo: args[2],
-				},
-			}
-			lastArgs := args[3:]
-			clientChainEarningAddress := &operatortypes.ClientChainEarningAddrList{}
-			clientChainEarningAddress.EarningInfoList = make([]*operatortypes.ClientChainEarningAddrInfo, 0)
-			for _, arg := range lastArgs {
-				strList := strings.Split(arg, ":")
-				if len(strList) != 2 {
-					return errorsmod.Wrap(operatortypes.ErrCliCmdInputArg, fmt.Sprintf("the error input arg is:%s", arg))
-				}
-				clientChainLzID, err := strconv.ParseUint(strList[0], 10, 64)
-				if err != nil {
-					return err
-				}
-				clientChainEarningAddress.EarningInfoList = append(clientChainEarningAddress.EarningInfoList,
-					&operatortypes.ClientChainEarningAddrInfo{
-						LzClientChainID: clientChainLzID, ClientChainEarningAddr: strList[1],
-					})
-			}
-			msg.Info.ClientChainEarningsAddr = clientChainEarningAddress
-			if err := msg.ValidateBasic(); err != nil {
+			txf, err := tx.NewFactoryCLI(clientCtx, cmd.Flags())
+			if err != nil {
 				return err
 			}
-			return tx.GenerateOrBroadcastTxCLI(cliCtx, cmd.Flags(), msg)
+
+			txf, msg, err := newBuildRegisterOperatorMsg(clientCtx, txf, cmd.Flags())
+			if err != nil {
+				return err
+			}
+
+			return tx.GenerateOrBroadcastTxWithFactory(clientCtx, txf, msg)
 		},
 	}
 
+	// EarningsAddr is the same as the sender's address, since the operator registration must be
+	// done by the operators themselves.
+
+	f := cmd.Flags()
+	// ApproveAddr may be different from the sender's address.
+	f.String(
+		FlagApproveAddr, "", "The address which is used to approve the delegations made to "+
+			"the operator. If not provided, it will default to the sender's address.",
+	)
+	// OperatorMetaInfo is the name of the operator.
+	f.String(
+		FlagMetaInfo, "", "The operator's meta info (like name)",
+	)
+	// clientChainLzID:ClientChainEarningsAddr
+	f.StringArray(
+		FlagClientChainData, []string{}, "The client chain's address to receive earnings; "+
+			"can be supplied multiple times. "+
+			"Format: <client-chain-id>:<client-chain-earnings-addr>",
+	)
+
+	// transaction level flags from the SDK
 	flags.AddTxFlagsToCmd(cmd)
+
+	// required flags
+	_ = cmd.MarkFlagRequired(FlagMetaInfo) // name of the operator
+
 	return cmd
+}
+
+func newBuildRegisterOperatorMsg(
+	clientCtx client.Context, txf tx.Factory, fs *flag.FlagSet,
+) (tx.Factory, *types.RegisterOperatorReq, error) {
+	sender := clientCtx.GetFromAddress()
+	// #nosec G701 // this only errors if the flag isn't defined.
+	approveAddr, _ := fs.GetString(FlagApproveAddr)
+	if approveAddr == "" {
+		approveAddr = sender.String()
+	}
+	metaInfo, _ := fs.GetString(FlagMetaInfo)
+	msg := &types.RegisterOperatorReq{
+		FromAddress: sender.String(),
+		Info: &types.OperatorInfo{
+			EarningsAddr:     sender.String(),
+			ApproveAddr:      approveAddr,
+			OperatorMetaInfo: metaInfo,
+		},
+	}
+	clientChainEarningAddress := &types.ClientChainEarningAddrList{}
+	// #nosec G701
+	ccData, _ := fs.GetStringArray(FlagClientChainData)
+	clientChainEarningAddress.EarningInfoList = make(
+		[]*types.ClientChainEarningAddrInfo, len(ccData),
+	)
+	for i, arg := range ccData {
+		strList := strings.Split(arg, ":")
+		if len(strList) != 2 {
+			return txf, nil, errorsmod.Wrapf(
+				types.ErrCliCmdInputArg, "the error input arg is:%s", arg,
+			)
+		}
+		// note that this is not the hex value but the decimal number.
+		clientChainLzID, err := strconv.ParseUint(strList[0], 10, 64)
+		if err != nil {
+			return txf, nil, errorsmod.Wrapf(
+				types.ErrCliCmdInputArg, "the error input arg is:%s", arg,
+			)
+		}
+		clientChainEarningAddress.EarningInfoList[i] = &types.ClientChainEarningAddrInfo{
+			LzClientChainID: clientChainLzID, ClientChainEarningAddr: strList[1],
+		}
+	}
+	msg.Info.ClientChainEarningsAddr = clientChainEarningAddress
+	return txf, msg, nil
 }
--- x/operator/types/msg.go
+++ x/operator/types/msg.go
@@ -5,15 +5,14 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 )
 
+// interface guards
 var (
 	_ sdk.Msg = &RegisterOperatorReq{}
-
-	// add for dogfood
 	_ sdk.Msg = &OptInToCosmosChainRequest{}
 	_ sdk.Msg = &InitOptOutFromCosmosChainRequest{}
 )
 
-// GetSigners returns the expected signers for a MsgUpdateParams message.
+// GetSigners returns the expected signers for the message.
 func (m *RegisterOperatorReq) GetSigners() []sdk.AccAddress {
 	addr := sdk.MustAccAddressFromBech32(m.FromAddress)
 	return []sdk.AccAddress{addr}
@@ -27,16 +26,13 @@ func (m *RegisterOperatorReq) ValidateBasic() error {
 	return nil
 }
 
-// GetSignBytes implements the LegacyMsg interface.
-func (m *RegisterOperatorReq) GetSignBytes() []byte {
-	return nil
-}
-
+// GetSigners returns the expected signers for the message.
 func (m *OptInToCosmosChainRequest) GetSigners() []sdk.AccAddress {
 	addr := sdk.MustAccAddressFromBech32(m.Address)
 	return []sdk.AccAddress{addr}
 }
 
+// ValidateBasic does a sanity check of the provided data
 func (m *OptInToCosmosChainRequest) ValidateBasic() error {
 	if _, err := sdk.AccAddressFromBech32(m.Address); err != nil {
 		return errorsmod.Wrap(err, "invalid from address")
@@ -44,11 +40,13 @@ func (m *OptInToCosmosChainRequest) ValidateBasic() error {
 	return nil
 }
 
+// GetSigners returns the expected signers for the message.
 func (m *InitOptOutFromCosmosChainRequest) GetSigners() []sdk.AccAddress {
 	addr := sdk.MustAccAddressFromBech32(m.Address)
 	return []sdk.AccAddress{addr}
 }
 
+// ValidateBasic does a sanity check of the provided data
 func (m *InitOptOutFromCosmosChainRequest) ValidateBasic() error {
 	if _, err := sdk.AccAddressFromBech32(m.Address); err != nil {
 		return errorsmod.Wrap(err, "invalid from address")
