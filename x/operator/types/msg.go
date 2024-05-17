11a8a960af2947c6e4a49de16cd9e968b917c030
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-05-17 04:06:05 +0000

    fix(operator): impl for msgs GetSignBytes, Type
    Without these functions implemented, sending a transaction with the
    wrong chain-id produces a panic while verifying the signature. This
    panic happens on the receiving node (and not the sender), although
    consensus isn't impacted.
    
    With these functions implemented, the receiving node replies "signature
    verification failed, please verify account number (%d) and chain-id
    (%s)". If the transaction were only legacy amino, even the "sequence
    (%d)" would have shown up in the error message.

--- x/operator/types/msg.go
+++ x/operator/types/msg.go
@@ -7,6 +7,19 @@ import (
 	"github.com/evmos/evmos/v14/types"
 )
 
+const (
+	// TypeRegisterOperatorReq is the type for the RegisterOperatorReq message.
+	TypeRegisterOperatorReq = "register_operator"
+	// TypeSetConsKeyReq is the type for the SetConsKeyReq message.
+	TypeSetConsKeyReq = "set_cons_key"
+	// TypeInitConsKeyRemovalReq is the type for the InitConsKeyRemovalReq message.
+	TypeInitConsKeyRemovalReq = "init_cons_key_removal"
+	// TypeOptIntoAVSReq is the type for the OptIntoAVSReq message.
+	TypeOptIntoAVSReq = "opt_into_avs"
+	// TypeOptOutOfAVSReq is the type for the OptOutOfAVSReq message.
+	TypeOptOutOfAVSReq = "opt_out_of_avs"
+)
+
 // interface guards
 var (
 	_ sdk.Msg = &RegisterOperatorReq{}
@@ -30,9 +43,19 @@ func (m *RegisterOperatorReq) ValidateBasic() error {
 	return m.Info.ValidateBasic()
 }
 
-// Route returns the transaction route. This must be specified for successful signing.
+// Route returns the transaction route.
 func (m *RegisterOperatorReq) Route() string {
 	return RouterKey
+}
+
+// Type returns the transaction type.
+func (m *RegisterOperatorReq) Type() string {
+	return TypeRegisterOperatorReq
+}
+
+// GetSignBytes returns the bytes all expected signers must sign over.
+func (m *RegisterOperatorReq) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
 }
 
 // GetSigners returns the expected signers for the message.
@@ -59,9 +82,19 @@ func (m *SetConsKeyReq) ValidateBasic() error {
 	return nil
 }
 
-// Route returns the transaction route. This must be specified for successful signing.
+// Route returns the transaction route.
 func (m *SetConsKeyReq) Route() string {
 	return RouterKey
+}
+
+// Type returns the transaction type.
+func (m *SetConsKeyReq) Type() string {
+	return TypeSetConsKeyReq
+}
+
+// GetSignBytes returns the bytes all expected signers must sign over.
+func (m *SetConsKeyReq) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
 }
 
 // GetSigners returns the expected signers for the message.
@@ -81,9 +114,19 @@ func (m *InitConsKeyRemovalReq) ValidateBasic() error {
 	return nil
 }
 
-// Route returns the transaction route. This must be specified for successful signing.
+// Route returns the transaction route.
 func (m *InitConsKeyRemovalReq) Route() string {
 	return RouterKey
+}
+
+// Type returns the transaction type.
+func (m *InitConsKeyRemovalReq) Type() string {
+	return TypeInitConsKeyRemovalReq
+}
+
+// GetSignBytes returns the bytes all expected signers must sign over.
+func (m *InitConsKeyRemovalReq) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
 }
 
 // GetSigners returns the expected signers for the message.
@@ -110,9 +153,19 @@ func (m *OptIntoAVSReq) ValidateBasic() error {
 	return nil
 }
 
-// Route returns the transaction route. This must be specified for successful signing.
+// Route returns the transaction route.
 func (m *OptIntoAVSReq) Route() string {
 	return RouterKey
+}
+
+// Type returns the transaction type.
+func (m *OptIntoAVSReq) Type() string {
+	return TypeOptIntoAVSReq
+}
+
+// GetSignBytes returns the bytes all expected signers must sign over.
+func (m *OptIntoAVSReq) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
 }
 
 // GetSigners returns the expected signers for the message.
@@ -140,4 +193,14 @@ func (m *OptOutOfAVSReq) ValidateBasic() error {
 // Route returns the transaction route. This must be specified for successful signing.
 func (m *OptOutOfAVSReq) Route() string {
 	return RouterKey
+}
+
+// Type returns the transaction type.
+func (m *OptOutOfAVSReq) Type() string {
+	return TypeOptOutOfAVSReq
+}
+
+// GetSignBytes returns the bytes all expected signers must sign over.
+func (m *OptOutOfAVSReq) GetSignBytes() []byte {
+	return sdk.MustSortJSON(ModuleCdc.MustMarshalJSON(m))
 }
