b2aef3c98bf2d5a39a417f9835888afc1eba0470
leonz789 <zleon789@gmail.com>
2024-06-13 23:39:32 +0800

    set first error code to 0

--- x/oracle/types/errors.go
+++ x/oracle/types/errors.go
@@ -8,7 +8,7 @@ import (
 
 // x/oracle module sentinel errors
 var (
-	ErrSample                     = sdkerrors.Register(ModuleName, 1100, "sample error")
+	ErrSample                     = sdkerrors.Register(ModuleName, 0, "sample error")
 	ErrInvalidMsg                 = sdkerrors.Register(ModuleName, 1, "invalid input create price")
 	ErrPriceProposalIgnored       = sdkerrors.Register(ModuleName, 2, "price proposal ignored")
 	ErrPriceProposalFormatInvalid = sdkerrors.Register(ModuleName, 3, "price proposal message format invalid")
