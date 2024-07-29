532c5994d224bf071614e6dee5fea3e5c6d57f1a
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-07-29 13:30:59 +0000

    refactor(avs): use IERC165 for blank contract

--- x/avs/types/types.go
+++ x/avs/types/types.go
@@ -5,12 +5,34 @@ import (
 
 	ibcclienttypes "github.com/cosmos/ibc-go/v7/modules/core/02-client/types"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/ethereum/go-ethereum/crypto"
 )
 
+// contract code used to generate ChainIDCode
+// It is a contract that supports IERC165 interface and does nothing else.
+// This way, contracts that follow the standard will not send ERC20/ERC721 transactions to the AVS.
+// 0.8.24+commit.e11b9ed9
+// 200 optimizer runs
+
+/*
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface IERC165 {
+    function supportsInterface(bytes4 interfaceId) external view returns (bool);
+}
+
+contract SupportsInterfaceExample is IERC165 {
+    function supportsInterface(bytes4 interfaceId) public pure override returns (bool) {
+        return interfaceId == type(IERC165).interfaceId;
+    }
+}
+*/
+
 var (
 	// ChainIDCode is the "fake" code used to mark a generated AVS address as occupied by a contract.
-	ChainIDCode = []byte("chain-id-code")
+	ChainIDCode = hexutil.MustDecode("0x608060405234801561001057600080fd5b5060c78061001f6000396000f3fe6080604052348015600f57600080fd5b506004361060285760003560e01c806301ffc9a714602d575b600080fd5b604e60383660046062565b6001600160e01b0319166301ffc9a760e01b1490565b604051901515815260200160405180910390f35b600060208284031215607357600080fd5b81356001600160e01b031981168114608a57600080fd5b939250505056fea2646970667358221220b872b230d6a37b4ce12f24d5127759bc0451696f0186fabee8c3e9abe32c462c64736f6c63430008180033")
 	// ChainIDCodeHash is the hash of the ChainIDCode.
 	ChainIDCodeHash = crypto.Keccak256Hash(ChainIDCode)
 	// ChainIDPrefix
