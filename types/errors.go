eb18ccbb16a60bd6fed056b5956002e9fb6f8df9
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-09-01 13:20:11 +0000

    fix: unnecessary parent code import

---
+++ types/errors.go
@@ -0,0 +1,13 @@
+package types
+
+import (
+	errorsmod "cosmossdk.io/errors"
+)
+
+// RootCodespace is the codespace for all errors defined in this package
+const RootCodespace = "exocore"
+
+// ErrInvalidChainID returns an error resulting from an invalid chain ID.
+var ErrInvalidChainID = errorsmod.Register(
+	RootCodespace, 3, "invalid chain ID",
+)
