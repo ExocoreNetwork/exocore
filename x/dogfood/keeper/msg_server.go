9b21e4825e3d7e2d425a5be1cfce72d3335f4a10
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-09-09 09:45:06 +0000

    fix build

--- x/dogfood/keeper/msg_server.go
+++ x/dogfood/keeper/msg_server.go
@@ -5,6 +5,7 @@ import (
 	"strings"
 
 	"cosmossdk.io/errors"
+	exocoretypes "github.com/ExocoreNetwork/exocore/types"
 	avskeeper "github.com/ExocoreNetwork/exocore/x/avs/keeper"
 	avstypes "github.com/ExocoreNetwork/exocore/x/avs/types"
 
@@ -110,9 +111,13 @@ func (k Keeper) UpdateParams(
 	k.SetParams(c, nextParams)
 
 	// update the related info in the AVS module
-	isAVS, avsAddr := k.avsKeeper.IsAVSByChainID(c, avstypes.ChainIDWithoutRevision(c.ChainID()))
+	isAVS, avsAddr := k.avsKeeper.IsAVSByChainID(
+		c, exocoretypes.ChainIDWithoutRevision(c.ChainID()),
+	)
 	if !isAVS {
-		return nil, errors.Wrapf(types.ErrNotAVSByChainID, "chainID:%s avsAddr:%s", c.ChainID(), avsAddr)
+		return nil, errors.Wrapf(
+			types.ErrNotAVSByChainID, "chainID:%s avsAddr:%s", c.ChainID(), avsAddr,
+		)
 	}
 	err := k.avsKeeper.UpdateAVSInfo(c, &avstypes.AVSRegisterOrDeregisterParams{
 		AvsName:           c.ChainID(),
