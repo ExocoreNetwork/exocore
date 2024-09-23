e418cbac54dd555832d2a16bf12aea30c09d5607
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-09-23 16:53:25 +0000

    refactor(delegation): respond to ai comments
    Instead of ignoring the error (even though we know it to be nil),
    examine the error and skip the loop if it is not nil. This is preferred
    over changing the function interface since it is more future proof.

--- x/delegation/keeper/abci.go
+++ x/delegation/keeper/abci.go
@@ -1,8 +1,6 @@
 package keeper
 
 import (
-	"strings"
-
 	assetstypes "github.com/ExocoreNetwork/exocore/x/assets/types"
 	"github.com/ExocoreNetwork/exocore/x/delegation/types"
 
@@ -13,92 +11,133 @@ import (
 
 // EndBlock : completed Undelegation events according to the canCompleted blockHeight
 // This function will be triggered at the end of every block, it will query the undelegation state to get the records that need to be handled and try to complete the undelegation task.
-func (k *Keeper) EndBlock(oCtx sdk.Context, _ abci.RequestEndBlock) []abci.ValidatorUpdate {
-	// #nosec G703 // the error is always nil
-	records, _ := k.GetPendingUndelegationRecords(oCtx, uint64(oCtx.BlockHeight()))
+func (k *Keeper) EndBlock(
+	originalCtx sdk.Context, _ abci.RequestEndBlock,
+) []abci.ValidatorUpdate {
+	logger := k.Logger(originalCtx)
+	records, err := k.GetPendingUndelegationRecords(
+		originalCtx, uint64(originalCtx.BlockHeight()),
+	)
+	if err != nil {
+		logger.Error("failed to get pending undelegation records", "error", err)
+	}
 	if len(records) == 0 {
+		logger.Info("no pending undelegation records")
 		return []abci.ValidatorUpdate{}
 	}
-	for _, record := range records {
-		ctx, writeCache := oCtx.CacheContext()
-		// check if the operator has been slashed or frozen
+	for i := range records {
+		record := records[i] // avoid implicit memory aliasing
+		ctx, writeCache := originalCtx.CacheContext()
+		// we can use `Must` here because we stored this record ourselves.
 		operatorAccAddress := sdk.MustAccAddressFromBech32(record.OperatorAddr)
-		// todo: don't think about freezing the operator in current implementation
-		/*		if k.slashKeeper.IsOperatorFrozen(ctx, operatorAccAddress) {
-				// reSet the completed height if the operator is frozen
-				record.CompleteBlockNumber = k.operatorKeeper.GetUnbondingExpirationBlockNumber(ctx, operatorAccAddress, record.BlockNumber)
-				if record.CompleteBlockNumber <= uint64(ctx.BlockHeight()) {
-					panic(fmt.Sprintf("the reset completedHeight isn't in future,setHeight:%v,curHeight:%v", record.CompleteBlockNumber, ctx.BlockHeight()))
-				}
-				_, innerError = k.SetSingleUndelegationRecord(ctx, record)
-				if innerError != nil {
-					panic(innerError)
-				}
-				continue
-			}*/
-
-		recordID := types.GetUndelegationRecordKey(record.BlockNumber, record.LzTxNonce, record.TxHash, record.OperatorAddr)
+		// TODO check if the operator has been slashed or frozen
+		recordID := types.GetUndelegationRecordKey(
+			record.BlockNumber, record.LzTxNonce, record.TxHash, record.OperatorAddr,
+		)
 		if k.GetUndelegationHoldCount(ctx, recordID) > 0 {
-			// delete it from state. then rewrite it with the next block
-			// #nosec G703 // the error is always nil
-			_ = k.DeleteUndelegationRecord(ctx, record)
-			// store it again with the next block and move on
+			// delete from all 3 states
+			if err := k.DeleteUndelegationRecord(ctx, record); err != nil {
+				logger.Error("failed to delete undelegation record", "error", err)
+				continue
+			}
+			// add back to all 3 states, with the new block height
 			// #nosec G701
 			record.CompleteBlockNumber = uint64(ctx.BlockHeight()) + 1
-			// we need to store two things here: one is the updated record in itself
-			// #nosec G703 // the error is always nil
-			recordKey, _ := k.SetSingleUndelegationRecord(ctx, record)
-			// and the other is the fact that it matures at the next block
-			// #nosec G703 // the error is always nil
-			_ = k.StorePendingUndelegationRecord(ctx, recordKey, record)
+			if err := k.SetUndelegationRecords(
+				ctx, []types.UndelegationRecord{*record},
+			); err != nil {
+				logger.Error("failed to set undelegation records", "error", err)
+				continue
+			}
 			writeCache()
 			continue
 		}
-		// TODO(mike): ensure that operator is required to perform self delegation to match above.
 
 		recordAmountNeg := record.Amount.Neg()
 		// update delegation state
 		deltaAmount := &types.DeltaDelegationAmounts{
 			WaitUndelegationAmount: recordAmountNeg,
 		}
-		_, err := k.UpdateDelegationState(ctx, record.StakerID, record.AssetID, record.OperatorAddr, deltaAmount)
-		if err != nil {
+		if _, err := k.UpdateDelegationState(
+			ctx, record.StakerID, record.AssetID, record.OperatorAddr, deltaAmount,
+		); err != nil {
 			// use oCtx so that the error is logged on the original context
-			k.Logger(oCtx).Error("failed to update delegation state", "error", err)
+			logger.Error(
+				"failed to update delegation state",
+				"error", err,
+			)
 			continue
 		}
 
 		// update the staker state
 		if record.AssetID == assetstypes.NativeAssetID {
-			parsedStakerID := strings.Split(record.StakerID, "_")
-			stakerAddr := sdk.AccAddress(hexutil.MustDecode(parsedStakerID[0]))
-			if err := k.bankKeeper.UndelegateCoinsFromModuleToAccount(ctx, types.DelegatedPoolName, stakerAddr, sdk.NewCoins(sdk.NewCoin(assetstypes.NativeAssetDenom, record.ActualCompletedAmount))); err != nil {
-				k.Logger(oCtx).Error("failed to undelegate coins from module to account", "error", err)
+			stakerAddrHex, _, err := assetstypes.ParseID(record.StakerID)
+			if err != nil {
+				logger.Error(
+					"failed to parse staker ID",
+					"error", err,
+				)
+				continue
+			}
+			stakerAddrBytes, err := hexutil.Decode(stakerAddrHex)
+			if err != nil {
+				logger.Error(
+					"failed to decode staker address",
+					"error", err,
+				)
+				continue
+			}
+			stakerAddr := sdk.AccAddress(stakerAddrBytes)
+			if err := k.bankKeeper.UndelegateCoinsFromModuleToAccount(
+				ctx, types.DelegatedPoolName, stakerAddr,
+				sdk.NewCoins(
+					sdk.NewCoin(assetstypes.NativeAssetDenom, record.ActualCompletedAmount),
+				),
+			); err != nil {
+				logger.Error(
+					"failed to undelegate coins from module to account",
+					"error", err,
+				)
 				continue
 			}
 		} else {
-			err = k.assetsKeeper.UpdateStakerAssetState(ctx, record.StakerID, record.AssetID, assetstypes.DeltaStakerSingleAsset{
-				WithdrawableAmount:        record.ActualCompletedAmount,
-				PendingUndelegationAmount: recordAmountNeg,
-			})
-			if err != nil {
-				k.Logger(oCtx).Error("failed to update staker asset state", "error", err)
+			if err := k.assetsKeeper.UpdateStakerAssetState(
+				ctx, record.StakerID, record.AssetID,
+				assetstypes.DeltaStakerSingleAsset{
+					WithdrawableAmount:        record.ActualCompletedAmount,
+					PendingUndelegationAmount: recordAmountNeg,
+				},
+			); err != nil {
+				logger.Error(
+					"failed to update staker asset state",
+					"error", err,
+				)
 				continue
 			}
 		}
 
 		// update the operator state
-		err = k.assetsKeeper.UpdateOperatorAssetState(ctx, operatorAccAddress, record.AssetID, assetstypes.DeltaOperatorSingleAsset{
-			PendingUndelegationAmount: recordAmountNeg,
-		})
-		if err != nil {
-			k.Logger(oCtx).Error("failed to update operator asset state", "error", err)
+		if err := k.assetsKeeper.UpdateOperatorAssetState(
+			ctx, operatorAccAddress, record.AssetID,
+			assetstypes.DeltaOperatorSingleAsset{
+				PendingUndelegationAmount: recordAmountNeg,
+			},
+		); err != nil {
+			logger.Error(
+				"failed to update operator asset state",
+				"error", err,
+			)
 			continue
 		}
 
 		// delete the Undelegation records that have been complemented
-		// #nosec G703 // the error is always nil
-		_ = k.DeleteUndelegationRecord(ctx, record)
+		if err := k.DeleteUndelegationRecord(ctx, record); err != nil {
+			logger.Error(
+				"failed to delete undelegation record",
+				"error", err,
+			)
+			continue
+		}
 		// when calling `writeCache`, events are automatically emitted on the parent context
 		writeCache()
 	}
--- x/delegation/types/msg.go
+++ x/delegation/types/msg.go
@@ -23,13 +23,18 @@ func (m *MsgDelegation) ValidateBasic() error {
 }
 
 // new message to delegate asset to operator
-func NewMsgDelegation(assetID, fromAddress string, amountPerOperator []KeyValue) *MsgDelegation {
+func NewMsgDelegation(
+	assetID string, fromAddress string, amountPerOperator []KeyValue,
+) *MsgDelegation {
 	baseInfo := &DelegationIncOrDecInfo{
 		FromAddress:        fromAddress,
 		PerOperatorAmounts: make([]KeyValue, 0, 1),
 	}
 	for _, kv := range amountPerOperator {
-		baseInfo.PerOperatorAmounts = append(baseInfo.PerOperatorAmounts, KeyValue{Key: kv.Key, Value: kv.Value})
+		baseInfo.PerOperatorAmounts = append(
+			baseInfo.PerOperatorAmounts,
+			KeyValue{Key: kv.Key, Value: kv.Value},
+		)
 	}
 	return &MsgDelegation{
 		AssetID:  assetID,
@@ -65,7 +70,10 @@ func NewMsgUndelegation(assetID, fromAddress string, amountPerOperator []KeyValu
 		PerOperatorAmounts: make([]KeyValue, 0, 1),
 	}
 	for _, kv := range amountPerOperator {
-		baseInfo.PerOperatorAmounts = append(baseInfo.PerOperatorAmounts, KeyValue{Key: kv.Key, Value: kv.Value})
+		baseInfo.PerOperatorAmounts = append(
+			baseInfo.PerOperatorAmounts,
+			KeyValue{Key: kv.Key, Value: kv.Value},
+		)
 	}
 	return &MsgUndelegation{
 		AssetID:  assetID,
@@ -73,18 +81,28 @@ func NewMsgUndelegation(assetID, fromAddress string, amountPerOperator []KeyValu
 	}
 }
 
-// TODO: delegation and undelegation have the same params, try to use one single message with different flag to indicate action:delegation/undelegation
+// validateDelegationInfo validates the delegation or undelegation info.
+// (1) the operator amounts are positive, and the operator addresses are valid.
+// (2) the assetID is native only, since only native token is supported for this mechanism.
+// (3) the from address is valid.
+// TODO: delegation and undelegation have the same params, try to use one single message with
+// different flag to indicate action:delegation/undelegation
 func validateDelegationInfo(assetID string, baseInfo *DelegationIncOrDecInfo) error {
 	for _, kv := range baseInfo.PerOperatorAmounts {
 		if _, err := sdk.AccAddressFromBech32(kv.Key); err != nil {
 			return errorsmod.Wrap(err, "invalid operator address delegateTO")
 		}
 		if !kv.Value.Amount.IsPositive() {
-			return errorsmod.Wrapf(ErrAmountIsNotPositive, "amount should be positive, got%s", kv.Value.Amount.String())
+			return ErrAmountIsNotPositive.Wrapf(
+				"amount should be positive, got %s", kv.Value.Amount.String(),
+			)
 		}
 	}
 	if assetID != assetstype.NativeAssetID {
-		return errorsmod.Wrapf(ErrInvalidAssetID, "only nativeToken is support, expected:%s,got:%s", assetstype.NativeAssetID, assetID)
+		return ErrInvalidAssetID.Wrapf(
+			"only nativeToken is support, expected:%s, got:%s",
+			assetstype.NativeAssetID, assetID,
+		)
 	}
 	if _, err := sdk.AccAddressFromBech32(baseInfo.FromAddress); err != nil {
 		return errorsmod.Wrap(err, "invalid from address")
