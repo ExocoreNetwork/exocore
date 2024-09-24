705bc146e5a19721bd13f4add58904462beef852
Mike <142298428+mikebraver@users.noreply.github.com>
2024-09-24 17:28:59 +0800

    update

--- x/reward/keeper/epoch_info.go
+++ x/reward/keeper/epoch_info.go
@@ -5,19 +5,21 @@ import (
 )
 
 // GetEpochIdentifier gets the epoch identifier
-func (k Keeper) GetEpochIdentifier(ctx sdk.Context) string {
+func (k Keeper) GetEpochIdentifier(_ sdk.Context) string {
+	// TODO: compatible with evmos v16
 	// store := ctx.KVStore(k.storeKey)
-	//bz := store.Get(types.KeyPrefixEpochIdentifier)
-	//if len(bz) == 0 {
-	//	return ""
-	//}
+	// bz := store.Get(types.KeyPrefixEpochIdentifier)
+	// if len(bz) == 0 {
+	// 	return ""
+	// }
 	//
-	//return string(bz)
+	// return string(bz)
 	return ""
 }
 
 // SetEpochsPerPeriod stores the epoch identifier
-func (k Keeper) SetEpochIdentifier(ctx sdk.Context, epochIdentifier string) {
-	//store := ctx.KVStore(k.storeKey)
-	//store.Set(types.KeyPrefixEpochIdentifier, []byte(epochIdentifier))
+func (k Keeper) SetEpochIdentifier(_ sdk.Context, _ string) {
+	// TODO: compatible with evmos v16
+	// store := ctx.KVStore(k.storeKey)
+	// store.Set(types.KeyPrefixEpochIdentifier, []byte(epochIdentifier))
 }
