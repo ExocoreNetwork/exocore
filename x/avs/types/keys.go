2f589af796c7b5bd806f256a0a0e2f594bf0adb3
trestin <trestin_blockchain@hotmail.com>
2024-09-26 14:09:53 +0800

    fix avs key

--- x/avs/types/keys.go
+++ x/avs/types/keys.go
@@ -16,8 +16,9 @@ const (
 	// RouterKey defines the module's message routing key
 	RouterKey = ModuleName
 )
+
 const (
-	prefixAVSInfo = iota + 1
+	prefixAVSInfo = iota + 5
 	prefixParams
 	prefixAVSTaskInfo
 	prefixOperatePub
