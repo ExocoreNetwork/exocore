20e7631a59f3d140bebc3dffe8ef0fddbb06a3e9
MaxMustermann2 <82761650+MaxMustermann2@users.noreply.github.com>
2024-10-16 11:57:51 +0000

    fix: GWT-01

--- x/reward/keeper/claim_reward.go
+++ x/reward/keeper/claim_reward.go
@@ -3,7 +3,6 @@ package keeper
 import (
 	"bytes"
 	"encoding/binary"
-	"fmt"
 	"log"
 	"math/big"
 	"strings"
@@ -125,32 +124,37 @@ func (k Keeper) PostTxProcessing(ctx sdk.Context, _ core.Message, receipt *ethty
 }
 
 func (k Keeper) RewardForWithdraw(ctx sdk.Context, event *RewardParams) error {
-	// check event parameter then execute RewardForWithdraw operation
-	if event.OpAmount.IsNegative() {
-		return errorsmod.Wrap(rtypes.ErrRewardAmountIsNegative, fmt.Sprintf("the amount is:%s", event.OpAmount))
-	}
-	stakeID, assetID := getStakeIDAndAssetID(event)
-	// check is asset exist
-	if !k.assetsKeeper.IsStakingAsset(ctx, assetID) {
-		return errorsmod.Wrap(rtypes.ErrRewardAssetNotExist, fmt.Sprintf("the assetID is:%s", assetID))
-	}
+	// TODO: rewards aren't yet supported
+	// it is safe to return an error, since the precompile call will prevent an error
+	// if err != nil return false
+	// the false will ensure no unnecessary LZ messages are sent by the gateway
+	return rtypes.ErrNotSupportYet
+	// // check event parameter then execute RewardForWithdraw operation
+	// if event.OpAmount.IsNegative() {
+	// 	return errorsmod.Wrap(rtypes.ErrRewardAmountIsNegative, fmt.Sprintf("the amount is:%s", event.OpAmount))
+	// }
+	// stakeID, assetID := getStakeIDAndAssetID(event)
+	// // check is asset exist
+	// if !k.assetsKeeper.IsStakingAsset(ctx, assetID) {
+	// 	return errorsmod.Wrap(rtypes.ErrRewardAssetNotExist, fmt.Sprintf("the assetID is:%s", assetID))
+	// }
 
-	// TODO
-	changeAmount := types.DeltaStakerSingleAsset{
-		TotalDepositAmount: event.OpAmount,
-		WithdrawableAmount: event.OpAmount,
-	}
-	// TODO: there should be a reward pool to be transferred from for native tokens' reward, don't update staker-asset-info, just transfer exo-native-token:pool->staker or handled by validators since the reward would be transferred to validators directly.
-	if assetID != types.ExocoreAssetID {
-		err := k.assetsKeeper.UpdateStakerAssetState(ctx, stakeID, assetID, changeAmount)
-		if err != nil {
-			return err
-		}
-		if err = k.assetsKeeper.UpdateStakingAssetTotalAmount(ctx, assetID, event.OpAmount); err != nil {
-			return err
-		}
-	}
-	return nil
+	// // TODO verify the reward amount is valid
+	// changeAmount := types.DeltaStakerSingleAsset{
+	// 	TotalDepositAmount: event.OpAmount,
+	// 	WithdrawableAmount: event.OpAmount,
+	// }
+	// // TODO: there should be a reward pool to be transferred from for native tokens' reward, don't update staker-asset-info, just transfer exo-native-token:pool->staker or handled by validators since the reward would be transferred to validators directly.
+	// if assetID != types.ExocoreAssetID {
+	// 	err := k.assetsKeeper.UpdateStakerAssetState(ctx, stakeID, assetID, changeAmount)
+	// 	if err != nil {
+	// 		return err
+	// 	}
+	// 	if err = k.assetsKeeper.UpdateStakingAssetTotalAmount(ctx, assetID, event.OpAmount); err != nil {
+	// 		return err
+	// 	}
+	// }
+	// return nil
 }
 
 // WithdrawDelegationRewards is an implementation of a function in the distribution interface.
